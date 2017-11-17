85c7740438b802e78491d0c3ce4515d7de622aa3
PialKanti <pialkanti2012@gmail.com>
2017-11-17 17:47:16 +0600

    initial commit

---
+++ app/src/main/java/com/pialkanti/architecuredemo/pojo/User.java
@@ -0,0 +1,28 @@
+package com.pialkanti.architecuredemo.pojo;
+
+/**
+ * @author: Pial Kanti Samadder<pialkanti2012@gmail.com>
+ * Date: 11/17/2017
+ * Time: 5:44 PM
+ */
+
+public class User {
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
